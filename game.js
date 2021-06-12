86695a5b4b347ccd71310d9bf3b60eefbd11419a
Nina Brissey <nbrissey@gmail.com>
2021-06-12 14:25:24 -0700

    Build basic structure for class and include possible methods without logic

---
+++ game.js
@@ -0,0 +1,24 @@
+class Game {
+  constructor() {
+    this.player1 = new Player('one', '<img src='assets/circle-transparent.png' alt='circle');
+    this.player2 = new Player('two', '<img src='assets/hexagon-transparent.png' alt='circle');
+  };
+
+  trackPlayerTurn() {
+
+  };
+
+  trackGameBoardPlays() {
+
+  };
+
+  checkWinConditions(player) {
+    //will check for win checkWinConditions
+    //maybe also add the draw here or in another function
+    //if win is true, player.wins++ (player referring to the player from the Player class)
+  };
+
+  resetGame() {
+
+  };
+};
