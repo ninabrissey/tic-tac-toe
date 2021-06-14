3baebc92e928de45cea891c07d4271398e0860dc
Nina Brissey <nbrissey@gmail.com>
2021-06-14 07:01:51 -0700

    Adjust saveWinsToStorage logic

--- player.js
+++ player.js
@@ -1,20 +1,28 @@
 class Player {
-  constructor(id, token) {
+  constructor(id, token, wins) {
     this.id = id;
     this.token = token;
-    this.wins = 0;
+    this.wins = wins || 0;
+    this.moves = [];
+    this.winner = false;
   };
 
+// this is saving all information. Is that what you want to do
   saveWinsToStorage() {
-    localStorage.clear();
     var key = JSON.stringify(this.id);
     var playerStringified = JSON.stringify(this);
+    localStorage.removeItem(key);
     localStorage.setItem(key, playerStringified);
   };
 
   retrieveWinsFromStorage() {
     var key = JSON.stringify(this.id);
     var retrievedWins = localStorage.getItem(key);
-    //will need to parseInt the value of the key to get the actual number 
+    //will need to parseInt the value of the key to get the actual number
   };
 };
+
+//what do I need to send to localStorage
+//what is the cleanest way to send everything to local storage
+
+//resinstantiate player one and two,
