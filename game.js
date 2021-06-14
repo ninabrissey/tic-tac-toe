a38057a43f82107f5ac815f27e5dc47408d6bb77
Nina Brissey <nbrissey@gmail.com>
2021-06-14 06:59:39 -0700

    Create logic for trackGameBoardPlays function, remove variable from function and add to properties

--- game.js
+++ game.js
@@ -3,7 +3,19 @@ class Game {
     this.playerOne = new Player('one', '<img class="game-board-piece" src="assets/circle-transparent.png" alt="circle">');
     this.playerTwo = new Player('two', '<img class="game-board-piece" src="assets/hexagon-transparent.png" alt="hexagon">');
     this.totalPlays = 0;
+    this.winner = false;
     this.currentMove;
+    this.draw;
+    this.winCombos = [
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 5, 9],
+      [3, 5, 7]
+    ];
   }
   //
   // trackGameBoardPlays() {
@@ -28,27 +40,19 @@ class Game {
   };
 
   checkWinConditions(player) {
-    var winCombos = [
-      [1, 4, 7],
-      [2, 5, 8],
-      [3, 6, 9],
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-      [1, 5, 9],
-      [3, 5, 7]
-    ];
-
-    for (var i = 0; i < winCombos.length; i++) {
-        if (player.moves.indexOf(winCombos[i][0], winCombos[i][1], winCombos[i][2]) === 2) {
-          console.log('winCombos', winCombos)
-          console.log(player.moves.indexOf(winCombos[i][0], winCombos[i][1], winCombos[i][2]))
-          console.log ('winner')
+    for (var i = 0; i < this.winCombos.length; i++) {
+        if (player.moves.includes(this.winCombos[i][0]) && player.moves.includes(this.winCombos[i][1]) && player.moves.includes(this.winCombos[i][2])) {
+        player.winner = true;
+        player.wins++;
+        console.log('winner')
+      } else {
+        this.draw = true;
+      }
     }
-  }
+  };
+
 };
 
-}
 
     //will check for win checkWinConditions
     //maybe also add the draw here or in another function
