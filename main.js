8fa8ff0afceec8ed4d338c95bde7bb91e3b034da
Nina Brissey <nbrissey@gmail.com>
2021-06-15 11:05:57 -0700

    -Adjust variables to match new id's -Add resetBoard function to Display winner function within setTimeout -Add show/hide Functions -Apply show/hide functions to resetBoard and displayWinner functions

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ var game = new Game();
 // Query Selectors 👇
 var gameBoard = document.getElementById('gameBoard');
 var buttons = document.querySelectorAll('.btn');
-var headerDisplay = document.getElementById('playerTurnIcon');
+var headerDisplay = document.getElementById('winnerDisplay');
 var playerOneWinCount = document.getElementById('playerOneWins');
 var playerTwoWinCount = document.getElementById('playerTwoWins');
+var playerTurnDisplay = document.getElementById('playerTurnDisplay');
+var winnerDisplay = document.getElementById('winnerDisplay');
+var winnerIcon = document.getElementById('winnerIcon');
+var playerIcon = document.getElementById('playerIcon');
 
 //Event Listeners 👇
 gameBoard.addEventListener('click', markTicOrTac);
@@ -20,7 +24,8 @@ function markTicOrTac(e) {
     displayToken(game.playerOne, e.target);
     game.checkWinConditions(game.playerOne);
     game.playerOne.saveWinsToStorage();
-    resetBoard();
+    playerIcon.src = "assets/hexagon-transparent.png";
+    displayWinner();
     game.resetGame();
     refreshWins();
     return;
@@ -31,7 +36,8 @@ function markTicOrTac(e) {
     displayToken(game.playerTwo, e.target);
     game.checkWinConditions(game.playerTwo);
     game.playerTwo.saveWinsToStorage();
-    resetBoard();
+    playerIcon.src = "assets/circle-transparent.png";
+    displayWinner();
     game.resetGame();
     refreshWins();
   }
@@ -43,33 +49,61 @@ function displayToken(player, element) {
 };
 
 function resetBoard() {
-  if (game.winner || game.draw) {
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].innerHTML = '';
-      buttons[i].disabled = false;
-      // Add the <h1 innerText here> to change the inner text
-    }
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].innerHTML = '';
+    buttons[i].disabled = false;
+    show(playerTurnDisplay);
+    hide(winnerDisplay);
+    playerIcon.src = "assets/circle-transparent.png";
   }
 };
 
 function refreshWins() {
   game.playerOne.retrieveWinsFromStorage();
   game.playerTwo.retrieveWinsFromStorage();
+  playerOneWinCount.innerText = `${game.playerOne.wins} wins`;
+  playerTwoWinCount.innerText = `${game.playerTwo.wins} wins`;
 };
 
-// function displayWinner ()
-//   if (game.winner && game.playerOne.isWinner) {
-//  headerDisplay.innerHTML = `<div>it's</div><img class="player-turn-icon" src="assets/circle-transparent.png" alt"circle"/><div>'s turn</div>`
-    // setTimeout(resetBoard, 3500);
-//     return;
-//   }
-//   if (game.winner && game.playerTwo.isWinner) {
-// headerDisplay.innerHTML = `<div>it's</div><img class="player-turn-icon" src="assets/hexagon-transparent.png" alt"hexagon"/><div>'s turn</div>`
-    // setTimeout(resetBoard, 3500);
-//     return;
-//   }
-//   if (game.draw && !game.winner) {
-// `<div>it's a draw... play again!</div>`
-    // setTimeout(resetBoard, 3500);
-//
-//   }
+function displayWinner() {
+  if (game.winner && game.playerOne.isWinner) {
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    show(winnerDisplay);
+    hide(playerTurnDisplay);
+    winnerIcon.src = 'assets/circle-transparent.png';
+    playerOneWinCount.innerText = `${game.playerOne.wins} wins`;
+    setTimeout(resetBoard, 3500);
+    return;
+  }
+  if (game.winner && game.playerTwo.isWinner) {
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    show(winnerDisplay);
+    hide(playerTurnDisplay);
+    winnerIcon.src = 'assets/hexagon-transparent.png';
+    playerTwoWinCount.innerText = `${game.playerTwo.wins} wins`;
+    setTimeout(resetBoard, 3500);
+    return;
+  }
+  if (game.totalPlays === 9) {
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    show(winnerDisplay);
+    hide(playerTurnDisplay);
+    winnerDisplay.innerHTML = '<div>it\'s a draw... play again!</div>';
+    setTimeout(resetBoard, 3500);
+    return
+  }
+};
+
+function show(element) {
+  element.classList.remove('hidden');
+};
+
+function hide(element) {
+  element.classList.add('hidden');
+};
