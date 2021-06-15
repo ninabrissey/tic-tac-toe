2ce9e687e5e6c8bcac25f5872348f2311e7556c7
Nina Brissey <nbrissey@gmail.com>
2021-06-14 19:35:38 -0700

    Finish method logic and add/adjust properties

--- player.js
+++ player.js
@@ -1,28 +1,29 @@
 class Player {
-  constructor(id, token, wins) {
+  constructor(id, token) {
     this.id = id;
     this.token = token;
-    this.wins = wins || 0;
+    this.wins = 0;
+    this.isWinner = false;
     this.moves = [];
-    this.winner = false;
   };
 
-// this is saving all information. Is that what you want to do
   saveWinsToStorage() {
-    var key = JSON.stringify(this.id);
-    var playerStringified = JSON.stringify(this);
-    localStorage.removeItem(key);
-    localStorage.setItem(key, playerStringified);
+    var playerId = JSON.stringify(this.id);
+    var winTotal = JSON.stringify(this.wins);
+    if (winTotal === 'null') {
+      winTotal = '0';
+    }
+    localStorage.setItem(playerId, winTotal);
   };
 
   retrieveWinsFromStorage() {
-    var key = JSON.stringify(this.id);
-    var retrievedWins = localStorage.getItem(key);
-    //will need to parseInt the value of the key to get the actual number
+    var playerId = JSON.stringify(this.id);
+    var retrievedWins = JSON.parse(localStorage.getItem(playerId));
+    this.wins = retrievedWins;
   };
+
+  // resetPlayer() {
+  //
+  // };
+
 };
-
-//what do I need to send to localStorage
-//what is the cleanest way to send everything to local storage
-
-//resinstantiate player one and two,
